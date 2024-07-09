ee4d2a9f59e6d31d508d2f40d243060e6dbe820e
ShahramShakiba <shahramshakiba88@gmail.com>
2024-07-09 19:49:52 +0330

    Add Resize Event file and listen to the resize

--- src/Experience/Experience.js
+++ src/Experience/Experience.js
@@ -1,3 +1,5 @@
+import Sizes from './Utils/Sizes';
+
 export default class Experience {
   constructor(canvas) {
     //======= Global access - like in terminal
@@ -5,5 +7,15 @@ export default class Experience {
 
     //======= Options
     this.canvas = canvas;
+
+    //======= Setup
+    this.sizes = new Sizes();
+
+    //=== Listen or register a callback for that event
+    this.sizes.on('resize', () => {
+      this.resize();
+    });
   }
+
+  resize() {}
 }
---
+++ src/Experience/Utils/Sizes.js
@@ -0,0 +1,21 @@
+import EventEmitter from './EventEmitter';
+
+export default class Sizes extends EventEmitter {
+  constructor() {
+    super();
+
+    this.width = window.innerWidth;
+    this.height = window.innerHeight;
+    this.pixelRatio = Math.min(window.devicePixelRatio, 2);
+
+    //======= Resize Event
+    window.addEventListener('resize', () => {
+      this.width = window.innerWidth;
+      this.height = window.innerHeight;
+      this.pixelRatio = Math.min(window.devicePixelRatio, 2);
+
+      //=== Trigger an event
+      this.trigger('resize');
+    });
+  }
+}
